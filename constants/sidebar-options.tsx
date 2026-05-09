ba3902c61a17d535fb075190184a66efb43658b7
Sandeep Singh <ssandeep8696@gmail.com>
2026-05-10 00:18:15 +0530

    feat: sidebar options single meta

--- constants/sidebar-options.tsx
+++ constants/sidebar-options.tsx
@@ -1,46 +1,15 @@
+import type { SidebarItemProps } from "@/types/docs/sidebar-types";
+import { LucideBarChart2 } from "lucide-react";
 
-import { SidebarOptionsProps } from "@/types/docs/sidebar-types";
-import {
-  LucideHome,
-  LucideBook,
-  LucideBarChart2,
-} from "lucide-react";
-
-export const SIDEBAR_OPTIONS: SidebarOptionsProps = {
-  gettingStarted: [
-    {
-      title: "Getting Started",
-      url: "/docs",
-      items: [
-        {
-          title: "Introduction",
-          url: "/docs",
-          icon: <LucideHome size={16} />,
-        },
-        {
-          title: "Prerequisites",
-          url: "/docs/prerequisites",
-          icon: <LucideBook size={16} />,
-        },
-      ],
-    },
-  ],
-  components: [
-    {
-      title: "All Components",
-      url: "#",
-      items: [
-        {
-          title: "Button",
-          url: "/docs/bar-charts",
-          icon: <LucideBarChart2 size={16} />,
-        },
-        {
-          title: "Input",
-          url: "/docs/animated-bar-charts",
-          icon: <LucideBarChart2 size={16} />,
-        },
-      ],
-    },
-  ],
-};
+export const SIDEBAR_OPTIONS: SidebarItemProps[] = [
+  {
+    title: "Button",
+    url: "/docs/bar-charts",
+    icon: <LucideBarChart2 size={16} />,
+  },
+  {
+    title: "Input",
+    url: "/docs/animated-bar-charts",
+    icon: <LucideBarChart2 size={16} />,
+  },
+];
